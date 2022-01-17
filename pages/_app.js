192295873e2a81c1a8459db23e3fc877f57bcd85
nisarg prajapati <nisarg.byajbook@gmail.com>
2022-01-17 20:48:21 +0530

    add new script

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,7 @@
 import postContext from "../store/posts";
 import "../styles/globals.css";
 import Head from "next/head";
+import Script from "next/script";
 
 function MyApp({ Component, pageProps }) {
   const ALL_POSTS = [
@@ -37,11 +38,11 @@ function MyApp({ Component, pageProps }) {
         />
       </Head>
       <Component {...pageProps} />
-      <script
+      <Script
         src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p"
         crossOrigin="anonymous"
-      ></script>
+      ></Script>
     </postContext.Provider>
   );
 }
